15dc16123b278e77c6711fcbe281c662c648daeb
acastemoreno <acastemoreno@gmail.com>
2015-12-04 16:58:49 -0500

    as

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('shelljs/global');
 
 app.get('/rest/:on/:tw/:th/:fo/:fi/:si/:se/:ei/:ni', function(req, res) {
 var text = exec('./sudokuexe ' + req.params.on + ' ' + req.params.tw + ' ' + req.params.th + ' ' + req.params.fo + ' ' + req.params.fi + ' ' + req.params.si + ' ' + req.params.se + ' ' + req.params.ei + ' ' + req.params.ni, {silent:true}).output;
+var origin = req.headers.origin;
+res.setHeader('Access-Control-Allow-Origin', origin);
 res.send(text);
 });
 
